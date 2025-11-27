30de29b1238c30d7777f31b8b79128f8e52230b2
Benedikt Schächner <benedikt.schaechner@gmail.com>
2025-11-27 20:04:19 +0000

    iframe.js:1 firebase-studio-1764234185695.cluster-ikslh4rdsnbqsvu5nw3v4

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
-import { Toaster } from "@/components/ui/toaster";
-import { Sonner } from "@/components/ui/sonner";
+import { Toaster } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
@@ -17,8 +16,7 @@ const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
       <AuthorProvider>
-        <Toaster />
-        <Sonner />
+        <Toaster richColors />
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<Index />} />
